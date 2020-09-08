b20f31be7da0493075691061dbcd684b6af431bb
karenanndonnachie <karenanndonnachie@gmail.com>
2020-09-08 23:18:07 +1000

    Update TextParaMobyDick.js

--- P5js/TextParaMobyDick/TextParaMobyDick.js
+++ P5js/TextParaMobyDick/TextParaMobyDick.js
@@ -1,9 +1,9 @@
 //working with midform text
 var myText
 function preload() {
-  myText = loadStrings('data/text.txt');
+  //myText = loadStrings('data/text.txt');
 }
-//var myText= "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball.";
+var myText= "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball.";
 
 
 function setup() {
@@ -19,8 +19,8 @@ function setup() {
 
 function draw() {
   background(255);
-  //textLeading(sin(frameCount*0.01)*50);
-  textLeading(mouseY/5);
+  textLeading(sin(frameCount*0.01)*50);
+  //textLeading(mouseY/5);
   text(myText, int(windowWidth/8), windowHeight/8, int(windowWidth/4*3), windowHeight);
   
 }
