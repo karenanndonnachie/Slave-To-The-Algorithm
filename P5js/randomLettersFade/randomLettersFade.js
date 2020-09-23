528d3e360753df9ae80d1eef17325312dd109e57
karenanndonnachie <karenanndonnachie@gmail.com>
2020-09-23 13:20:49 +1000

    Update randomLettersFade.js

--- P5js/randomLettersFade/randomLettersFade.js
+++ P5js/randomLettersFade/randomLettersFade.js
@@ -12,9 +12,10 @@ function setup() {
 
 
 function draw() {
+ background(255,1);
  rand = int(random(0, listLength-1));
  fill(0);
  textSize(random(5,50));
  text(letters[rand], random(width), random(height));
- background(255,5);
+
 }
